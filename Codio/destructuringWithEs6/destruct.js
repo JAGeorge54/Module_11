9fab7966262750b073f20d3e50fe06666506ce56
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-06 14:21:28 -0400

    added startercode

---
+++ Codio/destructuringWithEs6/destruct.js
@@ -0,0 +1,34 @@
+// Section 1: Array Destructuring
+
+const books = ["fiction", ["horror", "literary"], "science"];
+
+// TODO: Use destructuring to create the variables fiction horror literary and science
+
+////
+
+// Section 2: Object Destructuring
+
+const Student = {
+  firstName: "Joe",
+  university: "MIT",
+  studentInfo: {
+    studentid: 555,
+  },
+};
+
+// TODO: Use destructuring to create the variables firstName university and studentid
+
+////
+
+//don't change anything below this line 
+module.exports = {
+    fiction,
+    horror,
+    literary,
+    science,
+    firstName,
+    university,
+    studentid,
+  };
+
+
