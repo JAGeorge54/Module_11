3f65782290a93b799d3012d630e33eada35aaa93
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-08 16:02:10 -0400

    added starter code

---
+++ Codio/SpreadAndRestOperators/rest.js
@@ -0,0 +1,5 @@
+// TODO: create the function sumAll
+
+console.log(sumAll(1, 2)); // expected output: 3
+console.log(sumAll(1, 2, 3)); // expected output: 6
+console.log(sumAll(1, 2, 3, 4)); // expected output: 10
---
+++ Codio/SpreadAndRestOperators/spread.js
@@ -0,0 +1,4 @@
+const terrestrials = ["fox", "lion", "elephant"];
+const aquatics = ["shark", "whale", "octopus"];
+
+// TODO: create the array allAnimals that contains all animals from terrestrials and aquatics arrays
