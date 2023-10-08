796f55b8042a764aa276808c76e5e81310797112
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-08 17:47:18 -0400

    finished coding exercise

--- Codio/SpreadAndRestOperators/spread.js
+++ Codio/SpreadAndRestOperators/spread.js
@@ -2,3 +2,5 @@ const terrestrials = ["fox", "lion", "elephant"];
 const aquatics = ["shark", "whale", "octopus"];
 
 // TODO: create the array allAnimals that contains all animals from terrestrials and aquatics arrays
+allAnimals = [...terrestrials, ...aquatics];
+console.log(allAnimals);
