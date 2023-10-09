3d873783e6df235339b80d29d4acfad21a74dff5
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-10-09 09:18:10 -0400

    finished assignment

--- Codio/SpreadAndRestOperators/rest.js
+++ Codio/SpreadAndRestOperators/rest.js
@@ -1,5 +1,12 @@
 // TODO: create the function sumAll
-
+function sumAll(x, y, ...rest) {
+    let newList = [x, y, ...rest];
+    let add = 0;
+    for (let i = 0; i < newList.length; i++){
+         add += newList[i];
+    }
+    return add;
+}
 console.log(sumAll(1, 2)); // expected output: 3
 console.log(sumAll(1, 2, 3)); // expected output: 6
 console.log(sumAll(1, 2, 3, 4)); // expected output: 10
